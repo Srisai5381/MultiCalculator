1248c6a9993d2ecc833795bc43737ed25fddcf01
Srisai5381 <dandasrisai@gmail.com>
2024-06-05 22:24:13 -0400

    calculator

---
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.multicalculator.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.multicalculator.Greeting
+import com.example.multicalculator.Calculator
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingView(Calculator().add(5,7).toString())
+                    GreetingView(Calculator().subtract(8,4).toString())
+                    GreetingView(Calculator().multiply(2,4).toString())
+                    GreetingView(Calculator().divide(9,3).toString())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingView(text: String) {
+    Text(text = text)
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        GreetingView("Hello, Android!")
+    }
+}
